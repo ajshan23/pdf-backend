19e4ebab1ce5e17dacfd5e4f418df56f681ad168
ajshan23 <ajmalshahan23@gmail.com>
2025-02-21 22:37:21 +0530

    edited puppeter

--- index.js
+++ index.js
@@ -409,7 +409,9 @@ app.post("/generate-pdf", async (req, res) => {
 `
 
     // Launch Puppeteer and create PDF
-    const browser = await puppeteer.launch({ headless: "new" });
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
     const page = await browser.newPage();
     await page.setContent(htmlContent, { waitUntil: "load" });
     const pdfBuffer = await page.pdf({ format: "A4" });
